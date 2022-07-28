9c531763eb51ce8b0efa3b2dbb70916e6509df20
Edward Faulkner <edward@eaf4.com>
2022-07-28 14:01:27 -0400

    unmount on teardown

--- app/app/modifiers/react.js
+++ app/app/modifiers/react.js
@@ -1,11 +1,13 @@
 import Modifier from 'ember-modifier';
 import { createRoot } from 'react-dom/client';
 import { createElement } from 'react';
+import { registerDestructor } from '@ember/destroyable';
 
 export default class ReactModifier extends Modifier {
   modify(element, [reactComponent], props) {
     if (!this.root) {
       this.root = createRoot(element);
+      registerDestructor(this, () => this.root.unmount());
     }
     this.root.render(createElement(reactComponent, { ...props }));
   }
